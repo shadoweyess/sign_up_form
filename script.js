25dcc39087501b790ca744272a950f4099c59c5c
shadoweyess <klaudia.burkat@gmail.com>
2023-01-17 18:13:38 -0500

    index + js and css update

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function matchingPassword(){
-    let password = document.getElementById("user_password").value;
-    let confirmPassword = document.getElementById("confirm_password").value;
-    console.log(password,confirmPassword);
-    let message = document.getElementById("error");
-     
-    if (password.lenght != 0){
-        if (password == confirmPassword){
-            message.textContent = "";
-        }
-        else {
-            message.textContent = "*Password do not match"
-        }
-    }
-}
+
+
+
